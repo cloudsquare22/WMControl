707135a7e587cc51302089b3570886283c16036a
shi-n <shi-n_xyz@cloudsquare.jp>
2022-08-28 16:51:10 +0900

    Initial Commit

---
+++ WMControl/WMControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMControlApp.swift
+//  WMControl
+//
+//  Created by Shin Inaba on 2022/08/28.
+//
+
+import SwiftUI
+
+@main
+struct WMControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
